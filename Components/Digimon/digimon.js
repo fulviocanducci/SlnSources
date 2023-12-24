4f8d69f88df9a3c2d6f72214d33bc9cbf838fd59
Fulvio Cezar Canducci Dias <fulviocanducci@hotmail.com>
2023-12-24 13:12:06 -0300

    update digimon

--- Components/Digimon/digimon.js
+++ Components/Digimon/digimon.js
@@ -3,14 +3,15 @@ import axios from "axios";
 export default () => {
   return {
     items: null,
+    filter: "",
+    get filterItems() {
+      return this.items !== null && this.filter.length > 0 ? this.items.filter((x) => x.name.toLowerCase().includes(this.filter.toLowerCase())) : this.items;
+    },
     load() {
       axios.get("https://digitalinnovationone.github.io/api-digimon/api/digimon.json").then(
         (result) => {
-          console.log(result);
           if (result.status === 200) {
-            console.log(this.items);
             this.items = result.data;
-            console.log(this.items);
           } else {
             this.items = [];
           }
